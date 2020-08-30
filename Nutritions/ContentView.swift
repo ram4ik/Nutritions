8cb83f487e4013abfb87a3c6e0f9d9ab43f6598b
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-30 11:59:32 +0300

    Initial Commit

---
+++ Nutritions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nutritions
+//
+//  Created by Ramill Ibragimov on 30.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nutritions/NutritionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritionsApp.swift
+//  Nutritions
+//
+//  Created by Ramill Ibragimov on 30.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct NutritionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
